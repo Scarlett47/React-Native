6d5b25d29d1c1392d0a896bf59c3daf23debf95f
Scarlett <99087802+Scarlett47@users.noreply.github.com>
2023-01-31 12:51:29 +0800

    Sodoo

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.sodoo}>Hello</Text>
+      <Image style={{width:20,height:20}} source={require('./assets/favicon.png')}></Image>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sodoo:{
+    fontSize:"50px",
+    color:"red"
+  }
+});
