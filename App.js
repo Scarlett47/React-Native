8f6d957c772bd4a80ee93595aa86f2c08b1fbb0b
Scarlett47 <scarlett2037@gmail.com>
2023-02-28 13:05:56 +0800

    aa

--- App.js
+++ App.js
@@ -1,46 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, Pressable, StyleSheet, Text, View ,ScrollView} from 'react-native';
-import { TextInput } from 'react-native-web';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
+import { TextInput } from "react-native-web";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scroll}>
-      <Pressable onLongPress={()=>{
-        alert("Hello Image")
-      }} onPress={()=>{
-        alert('Image')
-      }}>
-        <Image style={{width:1000,height:1000}} source={require('./assets/bg.png')}></Image>
-      </Pressable>
-      <Pressable onLongPress={()=>{
-        alert("Hello World")
-      }} onPress={()=>{
-        alert('Hello')
-      }}>
-      <Text>Hello My name is Sod-Erdene</Text>
-      </Pressable>
-      {/* <TextInput
-        style={styles.inputs}
-        placeholder="Write..."
-      /> */}
-      <StatusBar style="auto" />
-      </ScrollView>
-    </View>
+    <>
+      <View style={styles.container}>
+        <ScrollView style={styles.scroll}>
+          {/* <Pressable
+          onLongPress={() => {
+            alert("Hello Image");
+          }}
+          onPress={() => {
+            alert("Image");
+          }}
+        >
+          <Image
+            style={{ width: 1000, height: 1000 }}
+            source={require("./assets/bg.png")}
+          ></Image>
+        </Pressable>
+        <Pressable
+          onLongPress={() => {
+            alert("Hello World");
+          }}
+          onPress={() => {
+            alert("Hello");
+          }}
+        >
+          <Text>Hello My name is Sod-Erdene</Text>
+        </Pressable>
+        <TextInput style={styles.inputs} placeholder="Write..." />
+        <StatusBar style="auto" /> */}
+          {/* <View style={styles.container}></View> */}
+        </ScrollView>
+      </View>
+      <View style={styles.container2}></View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height:1000,
+    backgroundColor: "gold",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  sodoo:{
-    fontSize:"50px",
-    color:"red"
+  container2: {
+    backgroundColor: "blue",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sodoo: {
+    fontSize: "50px",
+    color: "red",
   },
   inputs: {
     height: 40,
@@ -48,7 +70,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
-  scroll:{
-    marginHorizontal:20,
+  scroll: {
+    marginHorizontal: 20,
   },
 });
